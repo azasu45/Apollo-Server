4d76620581a72ddbfccb4e83fabdd768358f2d9f
unknown <corbos1995@gmail.com>
2022-04-21 23:55:21 -0400

    segundo commint

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
-import { gql,ApolloServer } from "apollo-server";
+import { gql, ApolloServer } from "apollo-server";
 
-const facturasCompras=[
-    {
-        fecha:'30/01/2022',
-        comprador:"comprador1",
-        documentoIdentidad:"0001",
-        id:"0000001"
-    },
-    {
-        fecha:'31/01/2022',
-        comprador:"comprador1",
-        documentoIdentidad:"0001",
-        id:"0000002"
-    },
-    {
-        fecha:'01/02/2022',
-        comprador:"comprador1",
-        documentoIdentidad:"0001",
-        id:"0000003"
-    },
-]
+const facturasCompras = [
+  {
+    fecha: "30/01/2022",
+    comprador: "comprador1",
+    documentoIdentidad: "0001",
+    id: "0000001",
+  },
+  {
+    fecha: "31/01/2022",
+    comprador: "comprador1",
+    documentoIdentidad: "0001",
+    id: "0000002",
+  },
+  {
+    fecha: "01/02/2022",
+    comprador: "comprador1",
+    documentoIdentidad: "0001",
+    id: "0000003",
+  },
+];
 
 const typeDefinidos = gql`
-    type facturasCompras{
-        fecha:String
-        comprador:String
-        documentoIdentidad:String
-        id:String!
-    }
+  type facturasCompras {
+    fecha: String
+    comprador: String
+    documentoIdentidad: String
+    id: String!
+  }
 
-    type Query {
-        cantidadFacturas:Int!
-        todasFacturasCompras:[facturasCompras]!
-    }
-`
+  type Query {
+    cantidadFacturas: Int!
+    todasFacturasCompras: [facturasCompras]!
+  }
+`;
 
-const resolver={
-    Query:{
-        cantidadFacturas:()=>facturasCompras.length,
-        todasFacturasCompras:()=> facturasCompras
-    }
-}
+const resolver = {
+  Query: {
+    cantidadFacturas: () => facturasCompras.length,
+    todasFacturasCompras: () => facturasCompras,
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs:typeDefinidos,
-    resolver
-})
+  typeDefs: typeDefinidos,
+  resolver,
+});
 
-server.listen().then(({url})=>{
-    console.log(`server in port ${url}`)
-})
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀  Server is ready at ${url}
+    📭  Query at https://studio.apollographql.com/dev`);
+});
